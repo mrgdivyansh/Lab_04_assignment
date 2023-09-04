237a0241e2be2fdf77e8c414b89c0c724a2627f2
mrgdivyansh <144017904+mrgdivyansh@users.noreply.github.com>
2023-09-04 20:48:01 +0530

    changes

--- import sys.py
+++ import sys.py
@@ -1,12 +1,13 @@
 import sys
 class Employee:
-    def __init__(self, name, age, salary):
+    def __init__(self, EId, name, age, salary):
+        self.EId=EId
         self.name = name
         self.age = age
         self.salary = salary
 
     def __repr__(self):
-        return f"Employee(name={self.name}, age={self.age}, salary={self.salary})"
+        return f"Employee(EId={self.EId},name={self.name}, age={self.age}, salary={self.salary})"
 
 def sort_by_age(employees):
     return sorted(employees, key=lambda x: x.age)
@@ -22,13 +23,12 @@ def print_employees(employees):
         print(employee)
 
 def main():
-    employees = [
-        Employee("John", 30, 50000),
-        Employee("Jane", 25, 40000),
-        Employee("Jack", 35, 60000),
-        Employee("Jill", 28, 45000)
-    ]
-
+    employees = [ 
+        Employee("161E90","Raman",41,56000),
+        Employee("161F91","Raman",41,56000),
+        Employee("161F99", "Jaya", 51, 82100),
+        Employee("171E20", "Tejas", 30, 55000)
+        Employee("171G30", "Ajay", 45, 44000)]
     print("Choose a sorting parameter:")
     print("1. Age")
     print("2. Name")
